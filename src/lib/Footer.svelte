5fce1ef09d58714b7f2f8a95e9fba8e35eacc63d
Durairaj Subramaniam <Durairaj.Subramaniam@gmail.com>
2022-08-04 22:47:48 +0100

    Added email

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   const siteMetadata = {
-    email: '',
+    email: 'durairaj@mahanthltd.co.uk',
     author: 'Mahanth Limited',
     github: 'https://github.com/durairaj',
     twitter: 'https://twitter.com/MahanthLimited',
