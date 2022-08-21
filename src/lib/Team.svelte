e3fd1d0558464d44ddf5cdebacc3dc3bcf7e6f2e
Durairaj Subramaniam <Durairaj.Subramaniam@gmail.com>
2022-08-21 13:33:34 +0100

    fix(): update theme

--- src/lib/Team.svelte
+++ src/lib/Team.svelte
@@ -1,127 +1,124 @@
 <script>
-      import Durai from '../assets/durai.jpeg';
-
+  import Durai from '../assets/durai.jpeg';
 </script>
+
 <section
   id="team"
-  class="dark:bg-solid-darkLighterBg bg-solid-lightgray py-16 grid grid-cols-1 lg:grid-cols-2 md:px-5 lg:px-16 defer rounded-br-6xl lg:bg-blocks-three bg-no-repeat bg-contain bg-right rtl:bg-left"
+  class="dark:bg-solid-darkLighterBg bg-solid-lightgray grid grid-cols-1 lg:grid-cols-2 md:px-5 lg:px-16 defer rounded-br-6xl lg:bg-blocks-three bg-no-repeat bg-contain bg-right rtl:bg-left"
 >
-<p
-class="text-base font-semibold leading-6 text-sky-500 dark:text-sky-400 uppercase"
->
-Team
-</p>
-</section>
-  <div
-    class="items-start space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0"
+  <p
+    class="text-base font-semibold leading-6 text-sky-500 dark:text-sky-400 uppercase"
   >
-    <div class="flex flex-col items-center pt-8">
-      <span
-        style="box-sizing: border-box; display: inline-block; overflow: hidden; width: initial; height: initial; background: none; opacity: 1; border: 0px; margin: 0px; padding: 0px; position: relative; max-width: 100%;"
-        ><span
-          style="box-sizing: border-box; display: block; width: initial; height: initial; background: none; opacity: 1; border: 0px; margin: 0px; padding: 0px; max-width: 100%;"
-          ><img
-            alt=""
-            aria-hidden="true"
-            src="data:image/svg+xml,%3csvg%20xmlns=%27http://www.w3.org/2000/svg%27%20version=%271.1%27%20width=%27192%27%20height=%27192%27/%3e"
-            style="display: block; max-width: 100%; width: initial; height: initial; background: none; opacity: 1; border: 0px; margin: 0px; padding: 0px;"
-          /></span
+    Team
+  </p>
+</section>
+<div class="items-start xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0">
+  <div class="flex flex-col items-center pt-8">
+    <span
+      style="box-sizing: border-box; display: inline-block; overflow: hidden; width: initial; height: initial; background: none; opacity: 1; border: 0px; margin: 0px; padding: 0px; position: relative; max-width: 100%;"
+      ><span
+        style="box-sizing: border-box; display: block; width: initial; height: initial; background: none; opacity: 1; border: 0px; margin: 0px; padding: 0px; max-width: 100%;"
         ><img
-          alt="avatar"
-          src={Durai}
-          decoding="async"
-          data-nimg="intrinsic"
-          class="h-48 w-48 rounded-full"
-          style="position: absolute; inset: 0px; box-sizing: border-box; padding: 0px; border: none; margin: auto; display: block; width: 0px; height: 0px; min-width: 100%; max-width: 100%; min-height: 100%; max-height: 100%;"
-        /><noscript /></span
-      >
-      <h3 class="pt-4 pb-2 text-2xl font-bold leading-8 tracking-tight">
-        Durairaj Subramaniam
-      </h3>
-      <div class="text-gray-500 dark:text-gray-400">
-        Technologist
-      </div>
-      <div class="text-gray-500 dark:text-gray-400">Director - Mahanth Limited</div>
-      <div class="flex space-x-3 pt-6">
-        <a
-          class="text-sm text-gray-500 transition hover:text-gray-600"
-          target="_blank"
-          rel="noopener noreferrer"
-          href="mailto:contact@mahanthltd.co.uk"
-          ><span class="sr-only">mail</span><svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            class="fill-current text-gray-700 hover:text-blue-500 dark:text-gray-200 dark:hover:text-blue-400 h-8 w-8"
-            ><path
-              d="M2.003 5.884 10 9.882l7.997-3.998A2 2 0 0 0 16 4H4a2 2 0 0 0-1.997 1.884z"
-            /><path
-              d="m18 8.118-8 4-8-4V14a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8.118z"
-            /></svg
-          ></a
-        ><a
-          class="text-sm text-gray-500 transition hover:text-gray-600"
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://github.com/durairaj"
-          ><span class="sr-only">github</span><svg
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-            class="fill-current text-gray-700 hover:text-blue-500 dark:text-gray-200 dark:hover:text-blue-400 h-8 w-8"
-            ><path
-              d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"
-            /></svg
-          ></a
-        ><a
-          class="text-sm text-gray-500 transition hover:text-gray-600"
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://www.linkedin.com/in/durairajsubramaniam/"
-          ><span class="sr-only">linkedin</span><svg
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-            class="fill-current text-gray-700 hover:text-blue-500 dark:text-gray-200 dark:hover:text-blue-400 h-8 w-8"
-            ><path
-              d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433a2.062 2.062 0 0 1-2.063-2.065 2.064 2.064 0 1 1 2.063 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"
-            /></svg
-          ></a
-        ><a
-          class="text-sm text-gray-500 transition hover:text-gray-600"
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://twitter.com/_londontrader_"
-          ><span class="sr-only">twitter</span><svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            class="fill-current text-gray-700 hover:text-blue-500 dark:text-gray-200 dark:hover:text-blue-400 h-8 w-8"
-            ><path
-              d="M23.953 4.57a10 10 0 0 1-2.825.775 4.958 4.958 0 0 0 2.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 0 0-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 0 0-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 0 1-2.228-.616v.06a4.923 4.923 0 0 0 3.946 4.827 4.996 4.996 0 0 1-2.212.085 4.936 4.936 0 0 0 4.604 3.417 9.867 9.867 0 0 1-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 0 0 7.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0 0 24 4.59z"
-            /></svg
-          ></a
-        >
-      </div>
+          alt=""
+          aria-hidden="true"
+          src="data:image/svg+xml,%3csvg%20xmlns=%27http://www.w3.org/2000/svg%27%20version=%271.1%27%20width=%27192%27%20height=%27192%27/%3e"
+          style="display: block; max-width: 100%; width: initial; height: initial; background: none; opacity: 1; border: 0px; margin: 0px; padding: 0px;"
+        /></span
+      ><img
+        alt="avatar"
+        src={Durai}
+        decoding="async"
+        data-nimg="intrinsic"
+        class="h-48 w-48 rounded-full"
+        style="position: absolute; inset: 0px; box-sizing: border-box; padding: 0px; border: none; margin: auto; display: block; width: 0px; height: 0px; min-width: 100%; max-width: 100%; min-height: 100%; max-height: 100%;"
+      /><noscript /></span
+    >
+    <h3 class="pt-4 pb-2 text-2xl font-bold leading-8 tracking-tight">
+      Durairaj Subramaniam
+    </h3>
+    <div class="text-gray-500 dark:text-gray-400">Technologist</div>
+    <div class="text-gray-500 dark:text-gray-400">
+      Director - Mahanth Limited
     </div>
-    <div class="prose max-w-none pt-8 pb-8 dark:prose-dark xl:col-span-2">
-        <a href="https://github.com/ryo-ma/github-profile-trophy"
-        ><img
-          src="https://github-profile-trophy.vercel.app/?username=durairaj"
-          alt="durairaj"
-        /></a
+    <div class="flex space-x-3 pt-6">
+      <a
+        class="text-sm text-gray-500 transition hover:text-gray-600"
+        target="_blank"
+        rel="noopener noreferrer"
+        href="mailto:contact@mahanthltd.co.uk"
+        ><span class="sr-only">mail</span><svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          class="fill-current text-gray-700 hover:text-blue-500 dark:text-gray-200 dark:hover:text-blue-400 h-8 w-8"
+          ><path
+            d="M2.003 5.884 10 9.882l7.997-3.998A2 2 0 0 0 16 4H4a2 2 0 0 0-1.997 1.884z"
+          /><path
+            d="m18 8.118-8 4-8-4V14a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8.118z"
+          /></svg
+        ></a
+      ><a
+        class="text-sm text-gray-500 transition hover:text-gray-600"
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://github.com/durairaj"
+        ><span class="sr-only">github</span><svg
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+          class="fill-current text-gray-700 hover:text-blue-500 dark:text-gray-200 dark:hover:text-blue-400 h-8 w-8"
+          ><path
+            d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"
+          /></svg
+        ></a
+      ><a
+        class="text-sm text-gray-500 transition hover:text-gray-600"
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://www.linkedin.com/in/durairajsubramaniam/"
+        ><span class="sr-only">linkedin</span><svg
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+          class="fill-current text-gray-700 hover:text-blue-500 dark:text-gray-200 dark:hover:text-blue-400 h-8 w-8"
+          ><path
+            d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433a2.062 2.062 0 0 1-2.063-2.065 2.064 2.064 0 1 1 2.063 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"
+          /></svg
+        ></a
+      ><a
+        class="text-sm text-gray-500 transition hover:text-gray-600"
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://twitter.com/_londontrader_"
+        ><span class="sr-only">twitter</span><svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          class="fill-current text-gray-700 hover:text-blue-500 dark:text-gray-200 dark:hover:text-blue-400 h-8 w-8"
+          ><path
+            d="M23.953 4.57a10 10 0 0 1-2.825.775 4.958 4.958 0 0 0 2.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 0 0-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 0 0-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 0 1-2.228-.616v.06a4.923 4.923 0 0 0 3.946 4.827 4.996 4.996 0 0 1-2.212.085 4.936 4.936 0 0 0 4.604 3.417 9.867 9.867 0 0 1-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 0 0 7.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0 0 24 4.59z"
+          /></svg
+        ></a
       >
-      <img
+    </div>
+  </div>
+  <div class="prose max-w-none pt-8 pb-8 dark:prose-dark xl:col-span-2">
+    <a href="https://github.com/ryo-ma/github-profile-trophy"
+      ><img
+        src="https://github-profile-trophy.vercel.app/?username=durairaj&include_all_commits=true&hide_border=true&margin-w=15&margin-h=15&column=3&theme=flat"
+        alt="durairaj"
+      /></a
+    >
+    <img
       align="left"
-      src="https://github-readme-stats.vercel.app/api/top-langs?username=durairaj&show_icons=true&locale=en&layout=compact"
+      src="https://github-readme-stats.vercel.app/api/top-langs?username=durairaj&show_icons=true&locale=en&include_all_commits=true&hide_border=true"
       alt="durairaj"
     />
     <img
       align="center"
-      src="https://github-readme-stats.vercel.app/api?username=durairaj&show_icons=true&locale=en"
+      src="https://github-readme-stats.vercel.app/api?username=durairaj&show_icons=true&locale=en&include_all_commits=true&hide_border=true"
       alt="durairaj"
     />
     <img
-    align="center"
-    src="https://github-readme-streak-stats.herokuapp.com/?user=durairaj&"
-    alt="durairaj"
-  />
-    </div>
+      align="center"
+      src="https://github-readme-streak-stats.herokuapp.com/?user=durairaj&include_all_commits=true&hide_border=true"
+      alt="durairaj"
+    />
   </div>
-
+</div>
